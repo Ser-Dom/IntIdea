23bef269c230cdf9d2f8ff2a777d35e4aba51635
Sergey Domoratski <drpiton@rambler.ru>
2020-07-24 15:45:22 +0300

    реализация вычисления расстояния между двумя точками в с пом класса

--- src/by/epam/learn/main/LearnMain.java
+++ src/by/epam/learn/main/LearnMain.java
@@ -6,27 +6,23 @@ public class LearnMain {
       hello("Иван");
       hello("User");
 
-      Square s = new Square(5);
-      System.out.println("площадь квадрата со стороной " + s.l + " = " + area(s));
+      Point s = new Point(2, 2, 7, 5);
+      System.out.println("Расстояние между точками x и y = " + s.area());
+
+      Square t = new Square(5);
+      System.out.println("площадь квадрата со стороной " + t.l + " = " + t.area());
 
       Rectangle r = new Rectangle(4, 6);
-      System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r));
+      System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 
       Triangle h = new Triangle(8, 4);
-      System.out.println("Площадь треугольника с основанием " + h.a + "и высотой " + h.b + " равна " + area(h));
+      System.out.println("Площадь треугольника с основанием " + h.a + "и высотой " + h.b + " равна " + h.area());
    }
+
    public static void hello(String somebody) {
       System.out.println("Hello, " + somebody + "!");
    }
-   public static double area(Square s) {
-      return s.l * s.l;
-   }
-   public static double area(Rectangle r) {
-      return r.a * r.b;
-   }
-   public  static double area(Triangle h){
-      return h.a*0.5*h.b;
-   }
+
 }
 
 
--- src/by/epam/learn/main/Triangle.java
+++ src/by/epam/learn/main/Triangle.java
@@ -1,12 +1,15 @@
 package by.epam.learn.main;
 
 public class Triangle {
+
    public double a;
    public double b;
 
    public Triangle(double a, double b){
       this.a=a;
       this.b=b;
-
+   }
+   public  double area(){
+      return this.a*0.5*this.b;
    }
 }
