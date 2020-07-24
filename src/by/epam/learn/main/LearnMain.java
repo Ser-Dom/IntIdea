e4a415dee02939309abfce9abaae3f346525b53f
Sergey Domoratski <drpiton@rambler.ru>
2020-07-24 14:09:30 +0300

    добвил еще клас
    добвил еще клас

--- src/by/epam/learn/main/LearnMain.java
+++ src/by/epam/learn/main/LearnMain.java
@@ -7,23 +7,26 @@ public class LearnMain {
       hello("User");
 
       Square s = new Square(5);
-      System.out.println("площадь квадрата со стороной " +s.l+ " = " + area(s));
+      System.out.println("площадь квадрата со стороной " + s.l + " = " + area(s));
 
       Rectangle r = new Rectangle(4, 6);
-           System.out.println("Площадь прямоугольника со сторонами " +r.a+ " и " +r.b+ " = " +area(r));
-   }
+      System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r));
 
+      Triangle h = new Triangle(8, 4);
+      System.out.println("Площадь треугольника с основанием " + h.a + "и высотой " + h.b + " равна " + area(h));
+   }
    public static void hello(String somebody) {
       System.out.println("Hello, " + somebody + "!");
    }
-
-   public static double area(Square s){
-      return s.l*s.l;
+   public static double area(Square s) {
+      return s.l * s.l;
    }
-   public static double area(Rectangle r){
-      return r.a*r.b;
+   public static double area(Rectangle r) {
+      return r.a * r.b;
    }
-
+   public  static double area(Triangle h){
+      return h.a*0.5*h.b;
+   }
 }
 
 
---
+++ src/by/epam/learn/main/Triangle.java
@@ -0,0 +1,12 @@
+package by.epam.learn.main;
+
+public class Triangle {
+   public double a;
+   public double b;
+
+   public Triangle(double a, double b){
+      this.a=a;
+      this.b=b;
+
+   }
+}
